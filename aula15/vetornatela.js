7cf72d7a98e4fc608e3d2979c7a38595c413fd95
Luan Victor <101013674+luanxvsm@users.noreply.github.com>
2023-08-29 20:05:08 -0300

    att

---
+++ aula15/vetornatela.js
@@ -0,0 +1,10 @@
+let valores = [8,3,6,9,1,4]
+
+//for(let pos=0; pos < valores.length; pos++){
+//    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+//}
+
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
